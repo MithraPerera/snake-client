e8a6f5901bd2220b2e64986acb3a94ffcbcbc3a3
Mithra Perera <mithraperera@hotmail.com>
2023-05-24 18:36:30 -0400

    handling user input to exit completed

--- play.js
+++ play.js
@@ -2,3 +2,28 @@ const { connect } = require("./client");
 
 console.log("Connecting ...");
 connect();
+
+// setup interface to handle user input from stdin
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  // Handling CTRL + C User Input
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+// Specifies what happens when data is recieved from stdin - pressing a key on the keyboard
+
+const handleUserInput = function(key) {
+  // your code here
+  // \u0003 maps to ctrl+c input
+  if (key === '\u0003') {
+    console.log("Exiting game. Goodbye!");
+    process.exit();
+  }
+};
+
+setupInput();
